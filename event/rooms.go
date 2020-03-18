750ab8d462364a1dc9446657d48a0a7830a5c2a6
CHAEUN PARK <codjs0910@naver.com>
2020-03-18 12:02:40 +0900

    get joined rooms
    Todo :
     - User type 생성

--- event/rooms.go
+++ event/rooms.go
@@ -33,3 +33,30 @@ func JoinRoom() (ret types.JoinRoomResp, err error) {
 	}
 	return ret, nil
 }
+
+func GetJoinedRooms() (ret types.JoinedRoomResp, err error) {
+	req, err := http.NewRequest("GET", config.Cfg.BaseUrl+"/joined_rooms", nil)
+	if err != nil {
+		return ret, fmt.Errorf("request create error : %s", err.Error())
+	}
+	//Todo : User Type 생성 후 AccessToken 및 UserId 가지고 있기
+	req.Header.Add("Authorization", config.Cfg.AccessTokenPrefix)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return ret, fmt.Errorf("send request error : %s", err.Error())
+	}
+	if resp.StatusCode == http.StatusOK {
+		if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
+			return ret, fmt.Errorf("response decode error : %s", err.Error())
+		}
+	} else {
+		errorRet := types.Error{}
+		if err := json.NewDecoder(resp.Body).Decode(&errorRet); err != nil {
+			return ret, fmt.Errorf("response decode error : %s", err.Error())
+		} else {
+			return ret, fmt.Errorf("get joined room falied ( status code : %d, error code : %s, error msg : %s )", resp.StatusCode, errorRet.Errcode, errorRet.Error)
+		}
+	}
+	return ret, nil
+}
--- types/rooms.go
+++ types/rooms.go
@@ -3,3 +3,7 @@ package types
 type JoinRoomResp struct {
 	RoomId string `json:"room_id"`
 }
+
+type JoinedRoomResp struct {
+	JoinedRoom []string `json:"joined_room"`
+}
