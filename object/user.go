b9dfca447d8797a4aff0e2893afe08797d1e6632
CHAEUN PARK <codjs0910@naver.com>
2020-03-19 16:34:34 +0900

    login test

---
+++ object/user.go
@@ -0,0 +1,45 @@
+package object
+
+import (
+	"fmt"
+	"github.com/CHAEUNPARK/simulator/event"
+)
+
+type User struct {
+	UserId      string `json:"user_id"`
+	AccessToken string `json:"access_token"`
+	Password    string `json:"password"`
+}
+
+func (user *User) Login() error {
+	result, err := event.Login(user.UserId, user.Password)
+	if err != nil {
+		return err
+	}
+	fmt.Println(result)
+	return nil
+}
+
+func (user *User) Logout() error {
+	return nil
+}
+
+func (user *User) SendMessage(msg string) error {
+	return nil
+}
+
+func (user *User) GetSync() error {
+	return nil
+}
+
+func (user *User) GetDevices() (ret []string, err error) {
+	return ret, nil
+}
+
+func (user *User) JoinRoom(roomId string) error {
+	return nil
+}
+
+func (user *User) GetJoinedRooms() (ret []string, err error) {
+	return ret, nil
+}
